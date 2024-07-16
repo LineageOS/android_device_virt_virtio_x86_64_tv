e5fa2cf7133f3f68bbc43356292d92318bfde196
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-17 00:13:35 +0800

    virtio_x86_64_tv: Add some more ATV related flags
    * Referenced from device/amlogic/yukawa/device-common.mk
    
    Change-Id: I66fee008d3cdbdfa8e7cf3e82b13fc9ca7a99c68

--- device.mk
+++ device.mk
@@ -10,6 +10,12 @@ PRODUCT_IS_ATV := true
 # Inherit from device
 $(call inherit-product, device/virt/virtio_x86_64/device.mk)
 
+# AAPT
+PRODUCT_AAPT_PREF_CONFIG := tvdpi
+
+# Characteristics
+PRODUCT_CHARACTERISTICS := tv
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
