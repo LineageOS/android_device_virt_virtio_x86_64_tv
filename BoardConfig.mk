24c446b54d3c4f263e4f77190ec91b9edc9eba67
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-11 16:12:37 +0800

    virtio_x86_64_tv: Move `USES_DEVICE_* := true` definition to top
    Change-Id: I057107c243fb399ad308dfa9026758483566c72c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+USES_DEVICE_VIRT_VIRTIO_X86_64_TV := true
+
 # Inherit from device
 include device/virt/virtio_x86_64/BoardConfig.mk
-
-USES_DEVICE_VIRT_VIRTIO_X86_64_TV := true
